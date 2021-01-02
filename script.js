331d4e7fbb5b1c3c992ec811153c6e56ff83c661
Youssef Ben Khalifa <64818777+itsyoussefbk@users.noreply.github.com>
2021-01-02 17:36:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,115 @@
+function change_1(){
+  var image_1 =document.getElementById('macquare');
+  var image_2 =document.getElementById('charles');
+  var image_3 =document.getElementById('newcastle');
+  var image_4 =document.getElementById('latrobe');
+  image_1.src =  "assets/images/macquare.png"
+  image_2.src = "assets/images/charles.png"
+  image_3.src = "assets/images/newcastle.png"
+  image_4.src = "assets/images/latrobe.png"
+}
+function change_2(){
+  var image_1 =document.getElementById('macquare');
+  var image_2 =document.getElementById('charles');
+  var image_3 =document.getElementById('newcastle');
+  var image_4 =document.getElementById('latrobe');
+
+  image_1.src =  "assets/images/sp1.png"
+  image_2.src = "assets/images/sp2.jpg"
+  image_3.src = "assets/images/sp3.jpg"
+  image_4.src = "assets/images/sp4.png"
+}
+function change_3(){
+  var image_1 =document.getElementById('macquare');
+  var image_2 =document.getElementById('charles');
+  var image_3 =document.getElementById('newcastle');
+  var image_4 =document.getElementById('latrobe');
+
+  image_1.src =  "assets/images/sp5.jpg"
+  image_2.src = "assets/images/sp6.png"
+  image_3.src = "assets/images/sp7.png"
+  image_4.src = "assets/images/sp8.png"
+}
+function change_4(){
+  var image_1 =document.getElementById('macquare');
+  var image_2 =document.getElementById('charles');
+  var image_3 =document.getElementById('newcastle');
+  var image_4 =document.getElementById('latrobe');
+
+  image_1.src =  "assets/images/sp9.jpg"
+  image_2.src = "assets/images/sp10.png"
+  image_3.src = "assets/images/sp11.jpg"
+  image_4.src = "assets/images/sp12.jpg"
+}
+
+function some_2(){
+  var text = document.getElementById("sometext");
+  var title = document.getElementById("u_1");
+  text.innerHTML="this is an example of what we can accomplish using javascript! it's amazing isn't it?";
+  title.innerHTML="New unisversity name in here"
+  document.getElementById("sometext_2").innerHTML="Sed aliquam posuere augue, vel ornare mi sollicitudin quis. Quisque congue venenatis ante, at consequat risus venenatis vel. Pellentesque porttitor venenatis ante, id egestas mauris"
+  document.getElementById("u_2").innerHTML="University Of Washington"
+  document.getElementById("months").innerHTML="Jan"
+  document.getElementById("days").innerHTML="29"
+  document.getElementById("jour").innerHTML="09"
+  document.getElementById("mois").innerHTML="Mar"
+
+
+
+}
+function some_3(){
+  var text = document.getElementById("sometext");
+  var title = document.getElementById("u_1");
+  text.innerHTML="Vestibulum congue nibh augue, eu tristique orci condimentum eget. Quisque in posuere orci. Nullam porttitor rhoncus laoreet. Praesent commodo enim id erat eleifend ultricies. Phasellus males";
+  title.innerHTML="University of Harvard"
+  document.getElementById("sometext_2").innerHTML="dio. Fusce malesuada elementum efficitur. Ut neque nisl, gravida id laoreet a, tincidunt quis nunc. Mauris vestibulum tristique nibh a facilisis. Pellentesque convallis lorem nec enim interdum blandit."
+  document.getElementById("u_2").innerHTML="University of Oxford"
+
+}
+function some_4(){
+  var text = document.getElementById("sometext");
+  var title = document.getElementById("u_1");
+  text.innerHTML="Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Mauris vitae odio eget odio blandit blandit a hendrerit nisi. Ut maximus dapibus urna eu suscipit. Proin bibendum quis lorem porta porta. Morbi eget nisi dui.";
+  title.innerHTML="University of Tunis"
+  document.getElementById("sometext_2").innerHTML="Suspendisse potenti. Curabitur malesuada justo vitae eleifend facilisis. In hac habitasse platea dictumst. Nullam suscipit mi tellus, a facilisis ligula luctus ut."
+  document.getElementById("u_2").innerHTML="University of Algeria"
+
+}
+function some_1(){
+  var text = document.getElementById("sometext");
+  var title = document.getElementById("u_1");
+  text.innerHTML="Maiores voluptas laboriosam non dolorum perferendis fuga repellat aut. Blanditiis quos in minus. vero ex possimus. Iure et consectetur";
+  title.innerHTML="Macquarie University, Sydney, Australia"
+  document.getElementById("sometext_2").innerHTML="Maiores voluptas laboriosam non s quos in minus. Voluptatum quia quia voluptas voluptatem vero ex possimus. Iure et consectetur"
+  document.getElementById("u_2").innerHTML="Federation University, Australia"
+  document.getElementById("months").innerHTML="Dec"
+  document.getElementById("days").innerHTML="23"
+  document.getElementById("jour").innerHTML="17"
+  document.getElementById("mois").innerHTML="Oct"
+
+}
+function image_changer_1(){
+  pic_1 = document.getElementById("img-1-src")
+  pic_2 = document.getElementById("img-2-src")
+  pic_1.src = 'assets/images/person-1.jpg'
+  pic_2.src = 'assets/images/person-2.jpg'
+  document.getElementById("first-name").innerHTML = "Harrison Phillips"
+  document.getElementById('first-title').innerHTML = "Reporter"
+  document.getElementById('first-des').innerHTML = 'Nisi cumque in necessitatibus molestiae eaque excepturi ab. Laboriosam ipsam voluptatem voluptatibus labore quam nihil. Quasi occaecati quos ratione quia aut molestiae velit et. Voluptatem id ea dicta inventore et quo impedit repudiandae. Qui'
+  document.getElementById("second-name").innerHTML = "Ainara Vergara"
+  document.getElementById('second-title').innerHTML = "Landscape Architect"
+  document.getElementById('second-des').innerHTML = 'Nisi cumque in necessitatibus molestiae eaque excepturi ab. Laboriosam ipsam voluptatem voluptatibus labore quam nihil. Quasi occaecati quos ratione quia aut molestiae velit et. Voluptatem id ea dicta inventore et quo impedit repudiandae. Qui' 
+}
+function image_changer_2(){
+  pic_1 = document.getElementById("img-1-src")
+  pic_2 = document.getElementById("img-2-src")
+  pic_1.src = 'assets/images/person-3.jpg'
+  pic_2.src = 'assets/images/person-4.webp'
+  document.getElementById("first-name").innerHTML = "Elon Musk"
+  document.getElementById('first-title').innerHTML = "CEO"
+  document.getElementById('first-des').innerHTML = 'Elon Musk  is a business magnate, industrial designer and engineer. He is the founder, CEO, CTO and chief designer of SpaceX; early investor, CEO and product architect of Tesla, Inc.'
+  document.getElementById("second-name").innerHTML = "Greta Thunberg"
+  document.getElementById('second-title').innerHTML = "Activist"
+  document.getElementById('second-des').innerHTML = 'Greta Tintin Eleonora Ernman Thunberg is a Swedish environmental activist who is internationally known for challenging world leaders to take immediate action against climate change' 
+
+}
